9efd52f48cfabd54074736c3f98d2079de604d34
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-11 00:39:41 -0600

    code complete.

--- Homework_Lists/Program.cs
+++ Homework_Lists/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Add students to a class roster List until there are no more students.
+// Then print out the count of students to the Console.
+
+List<string> roster = new List<string>();
+string name;
+
+do
+{
+    //ask for users name (exit to stop)
+    Console.Write("Add student to roster: ");
+    name = Console.ReadLine();
+    roster.Add(name);
+} while (name.ToLower() != "exit");
+
+Console.WriteLine(roster.Count());
